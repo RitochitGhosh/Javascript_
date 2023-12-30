c3755e1510e29193da70e7a89478ab9e50dad850
Ritochit Ghosh <145017372+RitochitGhosh@users.noreply.github.com>
2023-12-30 07:22:01 +0000

    Learned about variables in js

---
+++ Day_01/variables.js
@@ -0,0 +1,14 @@
+const accountId = 181238;
+let accountEmail = "ritochit@gmail.com";
+var accountPassword = "345981";
+accountCity = "kolkata";
+let accountState;
+
+//accountId = 846415 //Const elements can't be changed
+
+accountEmail = "rg_org@gmail.com";
+accountPassword = "464681";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
